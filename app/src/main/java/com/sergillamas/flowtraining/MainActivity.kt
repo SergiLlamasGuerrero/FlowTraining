45284c24664d5f92e452ec6851c4574171373c62
Sergi Llamas <sergi.llamas@scrm.lidl>
2023-03-14 14:08:11 +0100

    Create a button to start collect flow and show it into textView

--- app/src/main/java/com/sergillamas/flowtraining/MainActivity.kt
+++ app/src/main/java/com/sergillamas/flowtraining/MainActivity.kt
@@ -2,11 +2,12 @@ package com.sergillamas.flowtraining
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.launch
 
 class MainActivity : Activity() {
@@ -15,29 +16,14 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Transform list to flow
-        val flow1 = listOf(1, 2, 3, 4, 5, 6).asFlow()
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView)
 
-        // Create flow directly as list of elements
-        val flow2 = flowOf(1, 2, 3, 4, 5, 6)
-
-        // Create flow with block scope
-        val flow3 = makeFlow()
-
-        GlobalScope.launch {
-            println("@@ FLOW 1..............................")
-            flow1.collect {
-                println("@@ Value of emision: $it")
-            }
-            println("@@")
-            println("@@ FLOW 2..............................")
-            flow2.collect {
-                println("@@ Value of emision: $it")
-            }
-            println("@@")
-            println("@@ FLOW 3..............................")
-            flow3.collect {
-                println("@@ Value of emision: $it")
+        button.setOnClickListener {
+            GlobalScope.launch(Dispatchers.Main) {
+                makeFlow().collect { number ->
+                    textView.text = "${textView.text} $number"
+                }
             }
         }
     }
