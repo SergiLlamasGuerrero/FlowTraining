c04fec04735101ed25eed440532b279dfaa466bd
Sergi Llamas <sergi.llamas@scrm.lidl>
2023-03-14 17:29:28 +0100

    Use state flow to simulate pagination

--- app/src/main/java/com/sergillamas/flowtraining/MainActivity.kt
+++ app/src/main/java/com/sergillamas/flowtraining/MainActivity.kt
@@ -7,10 +7,16 @@ import android.widget.TextView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 
 class MainActivity : Activity() {
+
+    private val _stateFlow = MutableStateFlow("")
+    private val stateFlow: StateFlow<String> get() = _stateFlow
+    private var i = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,19 +25,26 @@ class MainActivity : Activity() {
         val button = findViewById<Button>(R.id.button)
         val textView = findViewById<TextView>(R.id.textView)
 
+        GlobalScope.launch(Dispatchers.Main) { stateFlow.collect { textView.text = it } }
+
         button.setOnClickListener {
-            GlobalScope.launch(Dispatchers.Main) {
-                makeFlow().collect { number ->
-                    textView.text = "${textView.text} $number"
+            GlobalScope.launch {
+                makeFlow().collect { text ->
+                    i++
+                    _stateFlow.value = "${textView.text}\n$text"
                 }
             }
         }
     }
 
     private fun makeFlow() = flow {
-        for (i in 0..10) {
-            delay(1_000)
-            emit(i)
+        delay(1_000)
+        val text = when (i) {
+            1 -> "FIRST PART OF TEXT"
+            2 -> "SECOND PART"
+            3 -> "LAST PART"
+            else -> ""
         }
+        emit(text)
     }
 }
